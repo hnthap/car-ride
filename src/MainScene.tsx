fc862fd3f8fd422247a89267b05c00ee72cd6d46
hnthap <94937751+hnthap@users.noreply.github.com>
2024-06-05 18:21:48 +0700

    wtv

--- src/MainScene.tsx
+++ src/MainScene.tsx
@@ -3,21 +3,17 @@ import {
   OrbitControls,
   PerspectiveCamera,
 } from "@react-three/drei";
-import { Suspense, useState } from "react";
-import { Vector3Array } from "./util";
+import { Suspense } from "react";
 import { Car, Ground, Landscape } from "./components";
 
 export default function MainScene() {
-  const [cameraPosition, setCameraPosition] = useState<Vector3Array>([
-    6, 6, -6.21,
-  ]);
   return (
     <Suspense fallback={null}>
       <Environment files={"/sky.jpg"} background={true} />
       <OrbitControls makeDefault rotateSpeed={0.5} />
-      <PerspectiveCamera makeDefault position={cameraPosition} fov={40} />
+      <PerspectiveCamera makeDefault position={[6, 6, -6.21]} fov={40} />
       <Landscape />
-      <Car setCameraPosition={setCameraPosition} />
+      <Car />
       <Ground />
     </Suspense>
   );
