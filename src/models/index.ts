77bd9c5273cab7a037fca3ac6fd2c58333227eb2
hnthap <94937751+hnthap@users.noreply.github.com>
2024-06-24 12:24:00 +0700

    Major changes: Remove unnecessary files: ES lint, postcss, tailwindcss, unused images and 3D models, pages (Home, About, Settings)

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,46 +1,26 @@
-import AdT from "./ArcdeTriumphe.jsx";
+import ArcDeTriomphe from "./ArcDeTriomphe.jsx";
 import Birch1 from "./Birch1.jsx";
-import Birch2 from "./Birch2.jsx";
-import Birch3 from "./Birch3.jsx";
-import Bugatti from "./Bugatti.jsx";
-import CarA from "./CarA.jsx";
-import DriftClashUluru from "./DriftClashUluru.jsx";
 import EiffelTower from "./EiffelTower.jsx";
 import FancyHouse from "./FancyHouse.jsx";
-import Ground from "./Ground.jsx";
-import { Pedestrian } from "./Kindred.jsx";
-import Lamborghini from "./Lamborghini.jsx";
 import ModernHouse from "./ModernHouse.jsx";
 import { Newground } from "./Newground.jsx";
 import { Model as NotreDameDeParis } from "./NotreDameDeParis.jsx";
 import Oggy from "./OggyHouse.jsx";
-import Sky from "./Sky.jsx";
 import SportCarRacingParis from "./SportCarRacingParis.jsx";
-import SportsCarRacingMoscow from "./SportsCarRacingMoscow.jsx";
 import VintageLantern from "./VintageLantern.jsx";
 import { WheelA } from "./WheelA.jsx";
 import { WheellessCar } from "./WheellessCar.jsx";
 
 export {
-  AdT,
+  ArcDeTriomphe,
   Birch1,
-  Birch2,
-  Birch3,
-  Bugatti,
-  CarA,
-  DriftClashUluru,
   EiffelTower,
   FancyHouse,
-  Ground,
-  Lamborghini,
   ModernHouse,
   Newground,
   NotreDameDeParis,
   Oggy,
-  Pedestrian,
-  Sky,
   SportCarRacingParis,
-  SportsCarRacingMoscow,
   VintageLantern,
   WheelA,
   WheellessCar
